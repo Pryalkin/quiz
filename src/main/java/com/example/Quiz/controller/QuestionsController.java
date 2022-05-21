81b5d3dad91b2374b5351b33da660c97c18a46b1
pryalkin <pppryalkin@gmail.com>
2022-05-21 22:55:20 +0300

    moved array to constants

--- src/main/java/com/example/Quiz/controller/QuestionsController.java
+++ src/main/java/com/example/Quiz/controller/QuestionsController.java
@@ -4,7 +4,6 @@ import com.example.Quiz.entity.Questions;
 import com.example.Quiz.service.QuestionService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -12,9 +11,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import static com.example.Quiz.constant.FileConstant.FORWARD_SLASH;
-import static com.example.Quiz.constant.FileConstant.USER_FOLDER;
-import static org.springframework.http.HttpStatus.OK;
+import static com.example.Quiz.constant.FileConstant.*;
 import static org.springframework.http.MediaType.IMAGE_JPEG_VALUE;
 
 @RequestMapping("/question")
@@ -25,27 +22,18 @@ public class QuestionsController {
 
     private final QuestionService questionService;
 
+
     @PostMapping("/saveImage")
     public void updateProfileImage(@RequestParam(value = "image") MultipartFile image,
                                    @RequestParam String name) throws IOException {
         questionService.saveImage(image, name);
     }
 
-
-
-
     @GetMapping("/{numberQuestion}")
     public Questions addUser(@PathVariable("numberQuestion") String numberQuestion){
         Long numberIdQuestion = Long.parseLong(numberQuestion);
         if (numberIdQuestion == 0){
-            String questions [][]= {{"0", "В какой стране придумали шаурму?", "В России", "В Германии", "В Лошице", "В Китае", "2"},
-                    {"1", "Кто написал книгу 'Война и мир'?", "Достоевский", "Сладкий", "Толстой", "Горький", "3"},
-                    {"2", "Что изображено на картинке?", "Цветочки", "Статуя свободы", "Бурдж-халифа", "Эйфелева башня", "4"},
-                    {"3", "Кто изображен на картинке?", "Киану Ривз", "Сильвестр Сталлоне", "Джонни Депп", "Пол Маккарти", "1"},
-                    {"4", "Что на картинке?", "Эспрессо", "Нескафе растворимый", "Каппучино", "Фредо", "3"},
-                    {"5", "Откуда родом мороженное?", "Из Китая", "Из Англии", "Из Бразилии", "Из Италии", "1"},
-                    {"6", "Что изображено на картинке?", "Омлет", "Борщ", "Щи", "Запеканка", "2"}};
-            questionService.createQuestions(questions);
+            questionService.createQuestions(QUESTIONS);
         }
        return questionService.getQuestion(Long.parseLong(numberQuestion));
     }
